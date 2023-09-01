0b9c52f22ba69ea8c9fde585ae9056fd3eb56e0e
Raihan Regita Harjuno <raihanregita.ppqita@gmail.com>
2023-09-01 20:59:32 +0700

    Hasil Akhir

--- last-project/src/App.jsx
+++ last-project/src/App.jsx
@@ -3,6 +3,8 @@ import './css/style1.css'
 import Hero from './components/Hero';
 import Pengantar from './components/Pengantar';
 import Contents from './components/Content';
+import Footer from './components/Footer';
+import Jenis from './components/Jenis';
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
       <Hero />
       <Pengantar />
       <Contents />
+      <Jenis />
+      <Footer />
     </div>
   )
 }
